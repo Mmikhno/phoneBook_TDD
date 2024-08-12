e69e25127a0191649d01b9fe59d73061ce20ee5e
Maria Mikhno <mmikhno89@gmail.com>
2024-08-12 19:06:34 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,5 @@
 public class PhoneBook {
+    public int add(String name, String lastName, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    void shouldAddNewPhoneNumber() {
+        phoneBook.add("Ivan", "Ivanov", "+12345698741");
+        phoneBook.add("Petr", "Petrov", "+98765432100");
+        int actual = phoneBook.add("Vasya", "Pupkin", "+98745632145");
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
 }
